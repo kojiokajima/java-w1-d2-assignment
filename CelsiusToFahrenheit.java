56f97ac2e248b52f9319329b491e9ffadec75b32
wanko <kouji124@w4.dion.ne.jp>
2020-10-07 02:21:43 +0900

    integrated declaration and definition

--- CelsiusToFahrenheit.java
+++ CelsiusToFahrenheit.java
@@ -4,15 +4,12 @@ public class CelsiusToFahrenheit {
 
     public static void main(String[] args) {
 
-        double celsius, fahrenheit;
-
         Scanner input = new Scanner(System.in);
 
         System.out.print("Please enter Celsius degree: ");
+        double celsius = input.nextDouble();
 
-        celsius = input.nextDouble();
-
-        fahrenheit = (9.0 / 5) * celsius + 32;
+        double fahrenheit = (9.0 / 5) * celsius + 32;
 
         System.out.println("It is " + fahrenheit + " degrees Fahrengeit.");
 
